66f55d9953fb0b5bfa8baa3159f85eb25c5fd410
kebard1988 <kebard1988@gmail.com>
2022-12-29 21:39:10 +0300

    первые данные

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,2 +1,16 @@
-class app {
+class App {
+//    создал переменую возраста
+    val id:Int = 34
+//    вывожу через метод mypri пееменную
+    fun mypri(){
+        println(id )
+    }
+
+}
+
+fun main(){
+//    создаю обьект
+    val mydan = App()
+    mydan.mypri()
+
 }
